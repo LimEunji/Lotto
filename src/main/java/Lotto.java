11529bbc8802113fd70512bafbeee8e7c8927935
EunjiLim <eunjilim@p-ful.com>
2019-02-27 12:05:31 +0900

    implement Lotto

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -1,10 +1,43 @@
 package main.java;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Lotto
 {
-	/** 6/45 **/
+	private final int count = 6;
+	private final int minNum = 1;
+	private final int maxNum = 45;
+
 	public int[] getNumbers()
 	{
-		return null;
+		int[] numbers = new int[count];
+		setNumber(numbers, 0);
+		return numbers;
+	}
+
+	private void setNumber(int[] result, int times)
+	{
+		if (times >= count) {
+			return;
+		}
+
+		int randomNum = ThreadLocalRandom.current().nextInt(minNum, maxNum + 1);
+		if (checkDuplicated(result, randomNum, times)) {
+			setNumber(result, times);
+		} else {
+			result[times] = randomNum;
+			setNumber(result, times + 1);
+		}
+	}
+
+	private boolean checkDuplicated(int[] result, int newNum, int times)
+	{
+		for (int i = 0; i < times - 1; i++) {
+			if (result[i] == newNum) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,29 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+
+		Scanner scanner = new Scanner(System.in);
+		String in;
+		Lotto lotto = new Lotto();
+		while (true) {
+			System.out.println("If you press 'n'+Enter, you will get the 6 lotto numbers!\n" +
+					"If you press any other with Enter, program will be exited.");
+
+			in = scanner.next();
+			if (!in.equals("n")) {
+				break;
+			}
+
+			int[] numbers = lotto.getNumbers();
+			for (int n : numbers) {
+				System.out.print(n + " ");
+			}
+
+			System.out.println();
+		}
 	}
 }
