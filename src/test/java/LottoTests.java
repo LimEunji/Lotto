4e1c3cb08d8980eaa630f0a87c8ac5d4bde06405
smilelife12 <31526631+smilelife12@users.noreply.github.com>
2019-03-07 11:47:40 +0900

    Update LottoTests.java

--- src/test/java/LottoTests.java
+++ src/test/java/LottoTests.java
@@ -1,8 +1,7 @@
 package test.java;
 
 import main.java.Lotto;
-import org.junit.jupiter.api
-		.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
